a9bbe867a8f5ce2f1b31b417fc1e063f172e59c5
DJGuruji <nath93266@gmail.com>
2024-10-27 12:35:18 +0530

    react icons

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AuthRouter from "./routers/AuthRouter";
 import "./App.css";
 
 function App() {
-  const isLogged = true; // Replace with actual logic to determine if the user is logged in
+  const isLogged = true; 
 
 
   return (
--- src/routers/HomeRouter.jsx
+++ src/routers/HomeRouter.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
-import Complaints from "../pages/Complaints";
+import React, { useState } from "react";
+import { Routes, Route, NavLink, useNavigate } from "react-router-dom";
 import Home from "../pages/Home";
+import Complaints from "../pages/Complaints";
 import Profile from "../pages/Profile";
 import Notification from "../pages/Notification";
 import Logs from "../pages/Logs";
@@ -11,9 +12,12 @@ import Users from "../pages/Users";
 import Activities from "../pages/Activities";
 import UserDetails from "../pages/UserDetails";
 import { ComplaintForm } from "../pages/ComplaintForm";
-import { Routes, Route, Link, useNavigate } from "react-router-dom";
 import musk from "/musk.jpg";
-import { FaBars } from 'react-icons/fa'; // Import menu icon
+import { FaBars } from 'react-icons/fa';
+import { FaHome } from "react-icons/fa";
+import { FaQuestionCircle } from "react-icons/fa";
+import { FaUserCircle } from "react-icons/fa";
+import { RiNotification3Fill } from "react-icons/ri";
 
 const HomeRouter = () => {
   const [dropdown, setDropdown] = useState(false);
@@ -23,16 +27,8 @@ const HomeRouter = () => {
   };
 
   const handleMenuItemClick = () => {
-    setDropdown(false); // Close dropdown when an item is clicked
+    setDropdown(false);
   };
-
-  const navigate = useNavigate();
-
-  // useEffect(() => {
-  //   if (window.location.pathname !== '/') {
-  //     navigate('/');
-  //   }
-  // }, [navigate]);
 
   return (
     <>
@@ -41,20 +37,53 @@ const HomeRouter = () => {
           <div className="text-lg font-bold">Logo</div>
           <div className="hidden md:flex">
             <ul className="flex space-x-4">
-              <li>
-                <Link to="/" className="hover:text-slate-500">Home</Link>
+              <li className="">
+                <NavLink 
+                  to="/" 
+                  className={({ isActive }) => 
+                    isActive ? 'flex underline text-slate-900 font-semibold ' : 'flex hover:text-slate-500'
+                  }
+                ><FaHome className="m-1 text-slate-900 "/>
+                  Home
+                </NavLink>
               </li>
               <li>
-                <Link to="complaints" className="hover:text-slate-500">Complaints</Link>
+                <NavLink 
+                  to="complaints" 
+                  className={({ isActive }) => 
+                    isActive ? 'flex underline text-slate-900 font-semibold' : 'flex hover:text-slate-500'
+                  }
+                ><FaQuestionCircle className="m-1 text-slate-900 " />
+                  Complaints
+                </NavLink>
               </li>
               <li>
-                <Link to="profile" className="hover:text-slate-500">Profile</Link>
+                <NavLink 
+                  to="profile" 
+                  className={({ isActive }) => 
+                    isActive ? 'flex underline text-slate-900 font-semibold' : 'flex hover:text-slate-500'
+                  }
+                ><FaUserCircle className="m-1 text-slate-900"/>
+                  Profile
+                </NavLink>
               </li>
               <li>
-                <Link to="notification" className="hover:text-slate-500">Notifications</Link>
+                <NavLink 
+                  to="notification" 
+                  className={({ isActive }) => 
+                    isActive ? 'flex underline text-slate-900 font-semibold' : 'flex hover:text-slate-500'
+                  }
+                ><RiNotification3Fill  className="m-1 text-slate-900"/>
+                  Notifications
+                </NavLink>
               </li>
               <li className="text-red-700">
-                <Link to="" className="hover:text-slate-500">Logout</Link>
+                <NavLink 
+                  to="" 
+                  className="hover:text-slate-500"
+                >
+                  Logout
+                </NavLink>
               </li>
               <li>
                 <img className="rounded-full w-14 h-14" src={musk} alt="Profile" />
@@ -63,25 +92,63 @@ const HomeRouter = () => {
           </div>
           <div className="md:hidden">
             <button className="text-2xl" onClick={handleDropdown}>
-              <FaBars /> {/* Menu icon */}
+              <FaBars />
             </button>
             {dropdown && (
               <div className="absolute top-20 right-0 mt-2 bg-gray-200 rounded-md shadow-lg w-48">
                 <ul className="flex flex-col text-lg">
                   <li>
-                    <Link to="/" onClick={handleMenuItemClick} className="block px-4 py-2 hover:bg-gray-300">Home</Link>
+                    <NavLink 
+                      to="/" 
+                      onClick={handleMenuItemClick} 
+                      className={({ isActive }) => 
+                        isActive ? 'block px-4 py-2 bg-gray-300' : 'block px-4 py-2 hover:bg-gray-300'
+                      }
+                    >
+                      Home
+                    </NavLink>
                   </li>
                   <li>
-                    <Link to="complaints" onClick={handleMenuItemClick} className="block px-4 py-2 hover:bg-gray-300">Complaints</Link>
+                    <NavLink 
+                      to="complaints" 
+                      onClick={handleMenuItemClick} 
+                      className={({ isActive }) => 
+                        isActive ? 'block px-4 py-2 bg-gray-300' : 'block px-4 py-2 hover:bg-gray-300'
+                      }
+                    >
+                      Complaints
+                    </NavLink>
                   </li>
                   <li>
-                    <Link to="profile" onClick={handleMenuItemClick} className="block px-4 py-2 hover:bg-gray-300">Profile</Link>
+                    <NavLink 
+                      to="profile" 
+                      onClick={handleMenuItemClick} 
+                      className={({ isActive }) => 
+                        isActive ? 'block px-4 py-2 bg-gray-300' : 'block px-4 py-2 hover:bg-gray-300'
+                      }
+                    >
+                      Profile
+                    </NavLink>
                   </li>
                   <li>
-                    <Link to="notification" onClick={handleMenuItemClick} className="block px-4 py-2 hover:bg-gray-300">Notifications</Link>
+                    <NavLink 
+                      to="notification" 
+                      onClick={handleMenuItemClick} 
+                      className={({ isActive }) => 
+                        isActive ? 'block px-4 py-2 bg-gray-300' : 'block px-4 py-2 hover:bg-gray-300'
+                      }
+                    >
+                      Notifications
+                    </NavLink>
                   </li>
                   <li>
-                    <Link to="" onClick={handleMenuItemClick} className="block px-4 py-2 text-red-700 hover:bg-gray-300">Logout</Link>
+                    <NavLink 
+                      to="" 
+                      onClick={handleMenuItemClick} 
+                      className="block px-4 py-2 text-red-700 hover:bg-gray-300"
+                    >
+                      Logout
+                    </NavLink>
                   </li>
                 </ul>
               </div>
@@ -94,22 +161,64 @@ const HomeRouter = () => {
         <aside className="fixed top-20 left-0 w-20 lg:w-48 xl:w-48 h-full bg-slate-100 border-r-2">
           <ul className="flex flex-col text-center text-lg space-y-10 mt-8">
             <li>
-              <Link to="users" className="block p-2 hover:bg-gray-200">Users</Link>
+              <NavLink 
+                to="users" 
+                className={({ isActive }) => 
+                  isActive ? 'underline text-slate-800 block p-2 bg-gray-200' : 'block p-2 hover:bg-gray-200'
+                }
+              >
+                Users
+              </NavLink>
             </li>
             <li>
-              <Link to="mybranch" className="block p-2 hover:bg-gray-200">My Branch</Link>
+              <NavLink 
+                to="mybranch" 
+                className={({ isActive }) => 
+                  isActive ? 'underline text-slate-800 block p-2 bg-gray-200' : 'block p-2 hover:bg-gray-200'
+                }
+              >
+                My Branch
+              </NavLink>
             </li>
             <li>
-              <Link to="settings" className="block p-2 hover:bg-gray-200">Settings</Link>
+              <NavLink 
+                to="settings" 
+                className={({ isActive }) => 
+                  isActive ? 'underline text-slate-800 block p-2 bg-gray-200' : 'block p-2 hover:bg-gray-200'
+                }
+              >
+                Settings
+              </NavLink>
             </li>
             <li>
-              <Link to="trash" className="block p-2 hover:bg-gray-200">Trash</Link>
+              <NavLink 
+                to="trash" 
+                className={({ isActive }) => 
+                  isActive ? 'underline text-slate-800 block p-2 bg-gray-200' : 'block p-2 hover:bg-gray-200'
+                }
+              >
+                Trash
+              </NavLink>
             </li>
             <li>
-              <Link to="logs" className="block p-2 hover:bg-gray-200">Logs</Link>
+              <NavLink 
+                to="logs" 
+                className={({ isActive }) => 
+                  isActive ? 'underline text-slate-800 block p-2 bg-gray-200' : 'block p-2 hover:bg-gray-200'
+                }
+              >
+                Logs
+              </NavLink>
             </li>
             <li>
-              <Link to="activities" className="block p-2 hover:bg-gray-200">Activities</Link>
+              <NavLink 
+                to="activities" 
+                className={({ isActive }) => 
+                  isActive ? 'underline text-slate-800 block p-2 bg-gray-200' : 'block p-2 hover:bg-gray-200'
+                }
+              >
+                Activities
+              </NavLink>
             </li>
           </ul>
         </aside>
